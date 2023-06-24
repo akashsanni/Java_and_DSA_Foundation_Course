95272f814546f27c150d3865f09159cb92a71a12
Akash kumar <akash8825317696@gmail.com>
2023-06-24 22:50:34 +0530

    initial commit

---
+++ Assignments/src/assignment1.java
@@ -0,0 +1,7 @@
+//Take 2 integer values in two variables x and y and print their product.
+public class assignment1 {
+    public static void main(String[] args) {
+        int x = 2, y=4;
+        System.out.println(x*y);
+    }
+}
