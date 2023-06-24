c5629f8d391c9d795aa7bfe28ce446553d466fc8
Akash kumar <akash8825317696@gmail.com>
2023-06-24 23:28:23 +0530

    commit 2

---
+++ src/Java - Printing(Output),variable , data types/question2.java
@@ -0,0 +1,7 @@
+public class question2 {
+    public static void main(String[] args) {
+        char character = 'U';
+        int asciiValue = character;
+        System.out.println(asciiValue);
+    }
+}
---
+++ src/Java - Printing(Output),variable , data types/question5.java
@@ -0,0 +1,19 @@
+public interface question5 {
+    public static void main(String[] args) {
+
+        int a = 5, b=6;
+
+        System.out.println("Before Swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        int temp=a;
+        a=b;
+        b=temp;
+
+
+
+        System.out.println("After Swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+    }
+}
